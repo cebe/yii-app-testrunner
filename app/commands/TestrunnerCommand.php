fc8093de40c326d828a6c6ff5305919ef38cba3c
Carsten Brandt <cbrandt@metadesign.de>
2011-03-08 17:30:03 +0100

    improved Testrunner Command
    - added --quiet --verbose and verbosity handling
    - added argument handling
    - added run logic

--- app/commands/TestrunnerCommand.php
+++ app/commands/TestrunnerCommand.php
@@ -7,9 +7,23 @@
  */
 class TestrunnerCommand extends CConsoleCommand
 {
-	public function actionHelp()
+	/**
+	 * verbosity value
+	 *
+	 * 0 = quiet
+	 *
+	 * @var
+	 */
+	public $verbose = 1;
+
+	/**
+	 * returns help string for command
+	 *
+	 * @return string
+	 */
+	public function getHelp()
 	{
-		echo <<<EOF
+		return <<<EOF
 yii app testrunner by Carsten Brandt (c) 2011
 ---------------------------------------------
 
@@ -22,29 +36,98 @@ HELP
 
 
 
-	listScopes  list of available scopes
+	listScopes  list of available scopes for --scope
 
+	runTests    run tests
 
+		--path          set TestCollector.basePath
+						default is the path defined in config/main.php
 
+		--scope         choose a scope to filter tests (default is all)
 
-	--scope     choose a scope to filter tests
+		--verbose=x     x=0 quiet
+						x=1 (default)
+						x>1 print more information
+
+		--quiet         like --verbose=0
 
 
 EOF;
 	}
 
-	public function actionIndex()
+	/**
+	 * testrunner help
+	 *
+	 * @return void
+	 */
+	public function actionHelp()
 	{
-		echo "running tests...";
-
-		//$tests = TestCollector();
-
-
-
-
-
+		echo $this->getHelp();
 	}
 
+	/**
+	 * testrunner default action
+	 *
+	 * @return void
+	 */
+	public function actionIndex()
+	{
+		echo $this->getHelp();
+	}
+
+	/**
+	 * runTests
+	 *
+	 * @return void
+	 */
+	public function actionRunTests($path=null, $scope='all', $verbose=1, $quiet=false)
+	{
+		// handle verbosity first
+		if ($quiet) {
+			$this->verbose = 0;
+		} else {
+			$this->verbose = $verbose;
+			if ($this->verbose > 1) {
+				$this->p('verbosity is ' . $this->verbose . "\n");
+			}
+		}
+
+		$this->p("preparing...\n", 2);
+
+		// scope
+		$this->p(" - scope is '$scope'\n", 2);
+
+		// basePath
+		if (!is_null($path)) {
+			if ($path{0} != '/') {
+				$path = $_SERVER['PWD'] . '/' . $path;
+			}
+			Yii::app()->testCollector->basePath = $path;
+		}
+		$this->p(" - base Path is '" . Yii::app()->testCollector->basePath . "'\n", 2);
+
+		$this->p("collecting tests...\n\n");
+
+		$collection = Yii::app()->testCollector->collectTests();
+
+		$this->p(" - found " . count($collection) /*->getCount()*/ . " tests\n", 1);
+
+		$this->p("filtering tests... \n\n");
+
+		$collection->applyScope($scope);
+
+		$this->p("running tests...\n\n");
+
+		$this->p("finished.\n");
+
+		$this->p("summary:");
+	}
+
+	/**
+     * list of available scopes
+	 *
+	 * @return void
+	 */
 	public function actionListScopes()
 	{
 		echo 'list of available scopes:' . "\n\n";
@@ -65,6 +148,19 @@ EOF;
 
 		echo "\n";
 		exit();
+	}
+
+	/**
+	 * print text based on verbosity
+	 *
+	 * @return void
+	 */
+	public function p($text, $verbose = 1)
+	{
+		if ($this->verbose >= $verbose) {
+			echo $text;
+		}
+
 	}
 
 }
