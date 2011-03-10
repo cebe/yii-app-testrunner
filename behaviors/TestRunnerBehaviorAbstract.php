c0fc0ea2c7e206af3919cab067d510ace3db3c7e
Carsten Brandt <cbrandt@metadesign.de>
2011-03-10 18:24:18 +0100

    added Abstract class for creating behaviors for TestRunner

---
+++ behaviors/TestRunnerBehaviorAbstract.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * behavior for testrunner
+ *
+ * @package Behaviors
+ */
+abstract class TestRunnerBehaviorAbstract extends CBehavior
+{
+	/**
+	 * register eventhandlers
+	 *
+	 * @return array
+	 */
+	public function events()
+	{
+		return array(
+			'onBeforeRun' => 'beforeRun',
+			'onBeforeTest' => 'beforeTest',
+			'onAfterTest' => 'afterTest',
+			'onAfterRun' => 'afterRun',
+		);
+	}
+
+	/**
+	 * Called before running a test sequence
+	 *
+	 * @param TestRunnerEvent the raised event holding the current testcollection
+	 * @return void
+	 */
+	public function beforeRun($event)
+	{
+
+	}
+
+	/**
+	 * Called before every single test run
+	 *
+	 * @param TestRunnerEvent the raised event holding the current testcollection and the test that will be run
+	 * @return void
+	 */
+	public function beforeTest($event)
+	{
+
+	}
+
+	/**
+	 * Called after every single test run
+	 *
+	 * @param TestRunnerEvent the raised event holding the current testcollection and the test that has been run
+	 * @return void
+	 */
+	public function afterTest($event)
+	{
+
+	}
+
+	/**
+	 * Called after running a test sequence
+	 *
+	 * @param TestRunnerEvent the raised event holding the current testcollection
+	 * @return void
+	 */
+	public function afterRun($event)
+	{
+
+	}
+}
