590bc8f6b26256f0e39cd393fdecfcf1d1bcec56
Carsten Brandt <cbrandt@metadesign.de>
2011-03-22 16:54:03 +0100

    added documentation to TestBase, enhanced attribute handling

--- models/TestBase.php
+++ models/TestBase.php
@@ -3,6 +3,12 @@
 /**
  * @author Carsten Brandt <mail@cebe.cc>
  * @package Tests
+ *
+ * @property-read boolean error true if error occured
+ * @property-read string  errorMessage
+ * @property-read boolean failure true if failure occured
+ * @property-read string  failureMessage
+ * @property-read boolean passed true if test passed
  */
 class TestBase extends TestAbstract
 {
@@ -23,6 +29,7 @@ class TestBase extends TestAbstract
 	public $results = null;
 
 	/**
+	 * get the value of an attribute or call corresponding getter method
 	 *
 	 * @return mixed
 	 */
@@ -36,14 +43,55 @@ class TestBase extends TestAbstract
 	}
 
 	/**
+	 * check whether an attribute or getter Method is existing
 	 *
-	 * @return mixed
+	 * @return boolean
 	 */
 	public function __isset($name)
 	{
-		return (isset($this->attributes[$name]) OR parent::__isset($name));
+		return $this->hasAttribute($name) OR parent::__isset($name);
 	}
 
+	/**
+	 * set the value of an (already existing) attribute or call corresponding setter method
+	 *
+	 * @return void
+	 */
+	public function __set($name, $value)
+	{
+		if (isset($this->attributes[$name])) {
+			$this->attributes[$name] = $value;
+		} else {
+			parent::__set($name, $value);
+		}
+	}
+
+	/**
+	 * add a new attribute, if it does not exist and
+	 * sets the value of this attribute
+	 *
+	 * @return void
+	 */
+	public function setAttribute($name, $value)
+	{
+		$this->attributes[$name] = $value;
+	}
+
+	/**
+	 * check whether this test has an attribute set
+	 *
+	 * @return boolean
+	 */
+	public function hasAttribute($name)
+	{
+		return isset($this->attributes[$name]);
+	}
+
+	/**
+	 * initialize the test object
+	 *
+	 * @return void
+	 */
 	public function init()
 	{
 		parent::init();
@@ -70,7 +118,13 @@ class TestBase extends TestAbstract
 		}
 	}
 
-
+	/**
+	 * run the test
+	 *
+	 * returns true on success, false on failure or error
+	 *
+	 * @return bool
+	 */
 	public function run()
 	{
 		// dependencies are handled by own behavior
@@ -92,11 +146,21 @@ class TestBase extends TestAbstract
 		return $result->wasSuccessful();
 	}
 
+	/**
+	 * true if test failed due to an error
+	 *
+	 * @return boolean
+	 */
 	public function getError()
 	{
 		return (count($this->results->errors()) > 0);
 	}
 
+	/**
+	 * returns the error message if tests failed due to an error
+	 *
+	 * @return string
+	 */
 	public function getErrorMessage()
 	{
 		$message = '';
@@ -106,11 +170,21 @@ class TestBase extends TestAbstract
 		return $message;
 	}
 
+	/**
+	 * true if test failed
+	 *
+	 * @return boolean
+	 */
 	public function getFailed()
 	{
 		return (count($this->results->failures()) > 0);
 	}
 
+	/**
+	 * returns the failure message if tests failed
+	 *
+	 * @return string
+	 */
 	public function getFailureMessage()
 	{
 		$message = '';
@@ -120,6 +194,11 @@ class TestBase extends TestAbstract
 		return $message;
 	}
 
+	/**
+	 * true if test passed
+	 *
+	 * @return boolean
+	 */
 	public function getPassed()
 	{
 		return !$this->getError() && !$this->getFailed();
