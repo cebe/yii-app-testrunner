54b8dfa935b9a53e5a8ea6f55374d033a65cc705
Carsten Brandt <cbrandt@metadesign.de>
2011-03-22 19:28:16 +0100

    refactor TestBase and TestAbstracts state handling
    - change behavior of test status attributes and messages

--- models/TestAbstract.php
+++ models/TestAbstract.php
@@ -1,25 +1,95 @@
 <?php
 
 /**
+ * This Test Class represents a Single Test Method
+ *
  * @author Carsten Brandt <mail@cebe.cc>
  * @package Tests
  *
- * @property-read string name
+ * @property-read string|boolean name
+ * @property-read boolean        error             true if error occured
+ * @property-read string|boolean errorMessage      the error Message
+ * @property-read boolean        failed            true if failure occured
+ * @property-read string|boolean failureMessage    the failure Message
+ * @property-read boolean        skipped           true if test is skipped
+ * @property-read string|boolean skippedMessage    the skipped Message
+ * @property-read boolean        incomplete        true if test is marked as incomplete
+ * @property-read string|boolean incompleteMessage the incomplete Message
+ * @property-read boolean        passed            true if test passed
  */
 abstract class TestAbstract extends CComponent
 {
+	/**
+	 * @var null|PHPUnit_Framework_TestCase
+	 */
 	public $testClass = null;
 
+	/**
+	 * @var null|ReflectionClass
+	 */
 	public $reflectionClass = null;
 
+	/**
+	 * @var null|ReflectionMethod
+	 */
 	public $testMethod = null;
 
-	public $docBlock = '';
 
+	/**
+	 * @var string
+	 */
 	private $_name = '';
+
+
+	/**
+	 * @var bool|string
+	 */
+	private $_error = false;
+
+	/**
+	 * @var bool|string
+	 */
+	private $_failed = false;
+
+	/**
+	 * @var bool|string
+	 */
+	private $_skipped = false;
+
+	/**
+	 * @var bool|string
+	 */
+	private $_incomplete = false;
+
+	/**
+	 * @var bool
+	 */
+	private $_passed = false;
+
+
+	/**
+	 * list of attributes assigned to this test
+	 *
+	 * @var array
+	 */
+	protected $attributes = array();
+
+
+	/**
+	 * return the name of this test
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->_name;
+	}
+
 
 	/**
 	 * will be called after construct
+	 *
+	 * overide this in your concrete class to do initializing stuff
 	 *
 	 * @return void
 	 */
@@ -29,25 +99,246 @@ abstract class TestAbstract extends CComponent
 	}
 
 	/**
-	 * return the name of this test
+	 * create a new instance with name, testclass and method name
 	 *
-	 * @return string
+	 * @param  $name
+	 * @param  $testClass
+	 * @param  $testMethod
 	 */
-	public function getName()
-	{
-		return get_class($this->testClass) . '::' . $this->_name;
-	}
-
 	public function __construct($name, $testClass, $testMethod)
 	{
 		$this->_name = $name;
 		$this->testClass = $testClass;
 		$this->reflectionClass = new ReflectionClass($testClass);
 		$this->testMethod = $this->reflectionClass->getMethod($testMethod);
-		$this->docBlock = $this->testMethod->getDocComment();
 
 		$this->init();
 	}
 
+
+	/**
+	 * get the value of an attribute or call corresponding getter method
+	 *
+	 * @return mixed
+	 */
+	public function __get($name)
+	{
+		if (isset($this->attributes[$name])) {
+			return $this->attributes[$name];
+		}
+
+		return parent::__get($name);
+	}
+
+	/**
+	 * check whether an attribute or getter Method is existing
+	 *
+	 * @return boolean
+	 */
+	public function __isset($name)
+	{
+		return $this->hasAttribute($name) OR parent::__isset($name);
+	}
+
+	/**
+	 * set the value of an (already existing) attribute or call corresponding setter method
+	 *
+	 * @return void
+	 */
+	public function __set($name, $value)
+	{
+		if (isset($this->attributes[$name])) {
+			$this->attributes[$name] = $value;
+		} else {
+			parent::__set($name, $value);
+		}
+	}
+
+	/**
+	 * add a new attribute, if it does not exist and
+	 * sets the value of this attribute
+	 *
+	 * @return void
+	 */
+	public function setAttribute($name, $value)
+	{
+		$this->attributes[$name] = $value;
+	}
+
+	/**
+	 * check whether this test has an attribute set
+	 *
+	 * @return boolean
+	 */
+	public function hasAttribute($name)
+	{
+		return isset($this->attributes[$name]);
+	}
+
+
+	/**
+	 * run the test
+	 *
+	 * returns true on success, false on failure or error
+	 *
+	 * @return bool
+	 */
 	abstract public function run();
+
+
+	/**
+	 * mark this test as "failed due to an error"
+	 *
+	 * @param string|boolean $message
+	 * @return void
+	 */
+	public function markError($message = 'Test failed due to an error.')
+	{
+		$this->_error = ($message === false OR is_string($message)) ? $message : '';
+	}
+
+	/**
+	 * true if test failed due to an error
+	 *
+	 * @return boolean
+	 */
+	public function getError()
+	{
+		return ($this->_error !== false);
+	}
+
+	/**
+	 * returns the error message if tests failed due to an error
+	 *
+	 * returns false, if test did not fail due to an error
+	 *
+	 * @return string|boolean
+	 */
+	public function getErrorMessage()
+	{
+		return $this->_error;
+	}
+
+
+	/**
+	 * mark this test as "failed"
+	 *
+	 * @param string|boolean $message
+	 * @return void
+	 */
+	public function markFailed($message = 'Test failed.')
+	{
+		$this->_failed = ($message === false OR is_string($message)) ? $message : '';
+	}
+
+	/**
+	 * true if test failed
+	 *
+	 * @return boolean
+	 */
+	public function getFailed()
+	{
+		return ($this->_failed !== false);
+	}
+
+	/**
+	 * returns the failure message if tests failed
+	 *
+	 * returns false, if test did not fail
+	 *
+	 * @return string|boolean
+	 */
+	public function getFailureMessage()
+	{
+		return $this->_failed;
+	}
+
+
+	/**
+	 * mark test as "skipped"
+	 *
+	 * @param string|boolean $message
+	 * @return void
+	 */
+	public function markSkipped($message = 'Test has been skipped.')
+	{
+		$this->_skipped = ($message === false OR is_string($message)) ? $message : '';
+	}
+
+	/**
+	 * true if test skipped
+	 *
+	 * @return boolean
+	 */
+	public function getSkipped()
+	{
+		return ($this->_skipped !== false);
+	}
+
+	/**
+	 * returns the skipped message if tests skipped
+	 *
+	 * returns false, if test has not been skipped
+	 *
+	 * @return string|boolean
+	 */
+	public function getSkippedMessage()
+	{
+		return $this->_skipped;
+	}
+
+
+	/**
+	 * mark test as "incomplete"
+	 *
+	 * @param string|boolean $message
+	 * @return void
+	 */
+	public function markIncomplete($message = 'Test has been skipped.')
+	{
+		$this->_incomplete = ($message === false OR is_string($message)) ? $message : '';
+	}
+
+	/**
+	 * true if test is incomplete
+	 *
+	 * @return boolean
+	 */
+	public function getIncomplete()
+	{
+		return ($this->_incomplete !== false);
+	}
+
+	/**
+	 * returns the incomplete message if test is incomplete
+	 *
+	 * returns false, if test is not incomplete
+	 *
+	 * @return string|boolean
+	 */
+	public function getIncompleteMessage()
+	{
+		return $this->_incomplete;
+	}
+
+
+	/**
+	 * mark this test as "passed"
+	 *
+	 * @return void
+	 */
+	public function markPassed()
+	{
+		$this->_passed = true;
+	}
+
+	/**
+	 * true if test passed
+	 *
+	 * @return boolean
+	 */
+	public function getPassed()
+	{
+		return ($this->_passed !== false);
+	}
 }
--- models/TestBase.php
+++ models/TestBase.php
@@ -4,22 +4,13 @@
  * @author Carsten Brandt <mail@cebe.cc>
  * @package Tests
  *
- * @property-read boolean error true if error occured
- * @property-read string  errorMessage
- * @property-read boolean failure true if failure occured
- * @property-read string  failureMessage
- * @property-read boolean passed true if test passed
  */
 class TestBase extends TestAbstract
 {
 	/**
-	 * list of attributes
-	 *
-	 * these attributes will be fetched from docbock annotations
-	 *
-	 * @var array
+	 * @var string
 	 */
-	protected $attributes = array();
+	public $docBlock = '';
 
 	/**
 	 * results
@@ -27,65 +18,6 @@ class TestBase extends TestAbstract
 	 * @var PHPUnit_Framework_TestResult
 	 */
 	public $results = null;
-
-	/**
-	 * get the value of an attribute or call corresponding getter method
-	 *
-	 * @return mixed
-	 */
-	public function __get($name)
-	{
-		if (isset($this->attributes[$name])) {
-			return $this->attributes[$name];
-		}
-
-		return parent::__get($name);
-	}
-
-	/**
-	 * check whether an attribute or getter Method is existing
-	 *
-	 * @return boolean
-	 */
-	public function __isset($name)
-	{
-		return $this->hasAttribute($name) OR parent::__isset($name);
-	}
-
-	/**
-	 * set the value of an (already existing) attribute or call corresponding setter method
-	 *
-	 * @return void
-	 */
-	public function __set($name, $value)
-	{
-		if (isset($this->attributes[$name])) {
-			$this->attributes[$name] = $value;
-		} else {
-			parent::__set($name, $value);
-		}
-	}
-
-	/**
-	 * add a new attribute, if it does not exist and
-	 * sets the value of this attribute
-	 *
-	 * @return void
-	 */
-	public function setAttribute($name, $value)
-	{
-		$this->attributes[$name] = $value;
-	}
-
-	/**
-	 * check whether this test has an attribute set
-	 *
-	 * @return boolean
-	 */
-	public function hasAttribute($name)
-	{
-		return isset($this->attributes[$name]);
-	}
 
 	/**
 	 * initialize the test object
@@ -96,6 +28,7 @@ class TestBase extends TestAbstract
 	{
 		parent::init();
 
+		$this->docBlock = $this->testMethod->getDocComment();
 		$this->parseDocBlock();
 	}
 
@@ -116,6 +49,16 @@ class TestBase extends TestAbstract
 				$this->attributes[$key] = $value;
 			}
 		}
+	}
+
+	/**
+	 * return the name of this test
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return get_class($this->testClass) . '::' . parent::getName();
 	}
 
 	/**
@@ -143,64 +86,46 @@ class TestBase extends TestAbstract
 
 		$this->results = $result;
 
-		return $result->wasSuccessful();
-	}
-
-	/**
-	 * true if test failed due to an error
-	 *
-	 * @return boolean
-	 */
-	public function getError()
-	{
-		return (count($this->results->errors()) > 0);
-	}
-
-	/**
-	 * returns the error message if tests failed due to an error
-	 *
-	 * @return string
-	 */
-	public function getErrorMessage()
-	{
-		$message = '';
-		foreach($this->results->errors() as $error) {
-			$message .= $error->getExceptionAsString() . "\n";
+		// evaluate results
+		if (count($this->results->errors()) > 0)
+		{
+			$message = '';
+			foreach($this->results->errors() as $error) {
+				$message .= $error->getExceptionAsString() . "\n";
+			}
+			$this->markError($message);
 		}
-		return $message;
-	}
-
-	/**
-	 * true if test failed
-	 *
-	 * @return boolean
-	 */
-	public function getFailed()
-	{
-		return (count($this->results->failures()) > 0);
-	}
-
-	/**
-	 * returns the failure message if tests failed
-	 *
-	 * @return string
-	 */
-	public function getFailureMessage()
-	{
-		$message = '';
-		foreach($this->results->failures() as $failure) {
-			$message .= $failure->getExceptionAsString() . "\n";
+		elseif (count($this->results->failures()) > 0)
+		{
+			$message = '';
+			foreach($this->results->failures() as $failure) {
+				$message .= $failure->getExceptionAsString() . "\n";
+			}
+			$this->markFailed($message);
 		}
-		return $message;
-	}
+		elseif (count($this->results->skipped()) > 0)
+		{
+			$message = '';
+			foreach($this->results->skipped() as $skip) {
+				$message .= $skip->getExceptionAsString() . "\n";
+			}
+			$this->markSkipped($message);
+		}
+		elseif (count($this->results->notImplemented()) > 0)
+		{
+			$message = '';
+			foreach($this->results->notImplemented() as $incomplete) {
+				$message .= $incomplete->getExceptionAsString() . "\n";
+			}
+			$this->markIncomplete($message);
+		}
+		elseif (count($this->results->passed()) > 0) {
+			$this->markPassed();
+		}
+		else {
+			throw new Exception('Unable to determine tests result of ' . $this->getName());
+		}
 
-	/**
-	 * true if test passed
-	 *
-	 * @return boolean
-	 */
-	public function getPassed()
-	{
-		return !$this->getError() && !$this->getFailed();
+		return $this->getPassed(); // @todo: think about senselessness
 	}
 }
